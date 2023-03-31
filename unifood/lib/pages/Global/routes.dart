7596d240d5ab3bfe1c59d967e00a604b8eabcb00
MierderTheKat <63218652+MierderTheKat@users.noreply.github.com>
2023-03-30 20:36:47 -0600

    Icono, Arguments y Borrar codigo basura
    Se agrego un icono a la aplicación.
    Se elimino el proyecto de pruebas y las pantallas de pruebas.
    Se añadió el paso de informacion entre pantallas de la parte de Cafetería, asi como la función de editar perfil.
    A su vez se elimino el código basura, o comentado para tener un ambiente mas limpio.

--- unifood/lib/pages/Global/routes.dart
+++ unifood/lib/pages/Global/routes.dart
@@ -8,9 +8,11 @@ export '/../firebase_options.dart';
 export 'package:cloud_firestore/cloud_firestore.dart';
 export 'package:firebase_storage/firebase_storage.dart';
 
-export 'package:image_cropper/image_cropper.dart';
-export 'package:fl_chart/fl_chart.dart';
 export 'package:image_picker/image_picker.dart';
+export 'package:image_cropper/image_cropper.dart';
+export 'package:flutter_image_compress/flutter_image_compress.dart';
+
+export 'package:fl_chart/fl_chart.dart';
 export 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 export '../Global/colores.dart';
@@ -36,12 +38,3 @@ export 'package:unifood/pages/U_Cafeteria/calendar_page.dart';
 export 'package:unifood/pages/U_Cafeteria/home_page.dart';
 export 'package:unifood/pages/U_Cafeteria/crud_page.dart';
 export 'package:unifood/pages/U_Cafeteria/order_page.dart';
-
-
-// Pruebas
-export 'package:unifood/pages/Pruebas/navigation.dart';
-
-export 'package:unifood/pages/Pruebas/pruebas_1.dart';
-export 'package:unifood/pages/Pruebas/pruebas_2.dart';
-export 'package:unifood/pages/Pruebas/pruebas_3.dart';
-export 'package:unifood/pages/Pruebas/pruebas_4_login.dart';
